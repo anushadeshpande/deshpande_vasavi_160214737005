43c9970a40f488f9b314e0bf3030664966b0f299
anushadeshpande <31873889+anushadeshpande@users.noreply.github.com>
2017-09-12 17:21:55 +0530

    1

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s =new Scanner(System.in);
+		Student s1=new Student();
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
